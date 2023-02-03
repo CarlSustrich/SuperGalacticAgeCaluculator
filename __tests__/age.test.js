f9d7d6fefc1b90b2e45186f78a318e771c790845
Carl Sustrich <carl.sustrich@gmail.com>
2023-02-03 08:51:43 -0800

    good fail test 2, string age

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -12,4 +12,9 @@ describe('Age Object Functionality', () => {
   test('it should take in a users age, and store it as an object property of number data type', () => {
     expect(newAge.currentEarthAge).toEqual(25);
   });
+
+  test('it should complete the last tests functionality, whether a string or number is passed in', () => {
+    let stringAge = new Age('25');
+    expect(stringAge.currentEarthAge).toEqual(25);
+  })
 });
