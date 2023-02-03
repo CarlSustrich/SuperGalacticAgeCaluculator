cc49065fa75a7902e7a8c26f936c247eea0477d4
Carl Sustrich <carl.sustrich@gmail.com>
2023-02-03 09:16:33 -0800

    pass test 5, mars age

--- src/age.js
+++ src/age.js
@@ -4,13 +4,16 @@ export class Age {
   }
 
   currentAgeOn(planet) {
-    const planetModifier = {mercury: 0.24, venus: 0.62};
+    const planetModifier = {mercury: 0.24, venus: 0.62, mars: 1.88};
     switch (planet) {
       case ('mercury'):
         this.mercuryAge = Math.floor(this.currentEarthAge / planetModifier[planet]);
         break;
       case ('venus'):
         this.venusAge = Math.floor(this.currentEarthAge / planetModifier[planet]);
+        break;
+      case ('mars'):
+        this.marsAge = Math.floor(this.currentEarthAge / planetModifier[planet]);
     }
   }
 }
